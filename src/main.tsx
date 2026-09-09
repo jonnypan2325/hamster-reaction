a7abd9f38e4b5070877a20357876d796b18e3f1b
Jonathan Pan <130952971+jonnypan2325@users.noreply.github.com>
2026-09-08 21:01:13 -0700

    build: set up react and typescript with vite

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <main>
+      <h1>Hamster Reaction</h1>
+    </main>
+  </React.StrictMode>,
+);
